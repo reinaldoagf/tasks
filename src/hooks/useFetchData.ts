015647a09162bf044de1b558058d94717c7aadd5
reinaldoagf <reinaldoagf1@gmail.com>
2024-06-05 23:58:51 -0400

    feat: ListScreen with useFetchData hook

---
+++ src/hooks/useFetchData.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetchData = (url: string) => {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError('Failed to fetch data');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetchData;
--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,10 +1,75 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
+import useFetchData from '../hooks/useFetchData';
+import moment from 'moment'
+
+
 const ListScreen = () => {
+    const { data, loading, error } = useFetchData('https://6172cfe5110a740017222e2b.mockapi.io/elements');
+
+    if (loading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
+
+    if (error) {
+        return (
+            <View style={styles.centered}>
+                <Text style={styles.errorText}>{error}</Text>
+            </View>
+        );
+    }
+
     return (
-        <View>
-            <Text>ListScreen</Text>
+        <View style={styles.container}>
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.itemContainer}>
+                        <Text style={[styles.itemText, {fontWeight: 'semibold'}]}>{item.name}</Text>
+                        <Text style={styles.itemText}>{moment(item.createdAt).format("DD/mm/yyyy h:mA")}</Text>
+                    </View>
+                )}
+            />
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: '#091a2e',
+    },
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#091a2e',
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 18,
+    },
+    itemContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#172b46',
+        borderRadius: 10,
+        marginBottom: 10,
+        gap: 5
+    },
+    itemText: {
+        fontSize: 18,
+        color: '#51d7bc',
+        fontWeight: 'bold',
+    },
+});
+
 export default ListScreen;
